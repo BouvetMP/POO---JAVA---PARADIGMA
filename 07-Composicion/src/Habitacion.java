d8008bac1e7b9556f4885ccf3eebc825c497fee9
MYFINGER <julianalberto448@gmail.com>
2026-04-10 22:12:06 -0500

    07 Hecho

---
+++ 07-Composicion/src/Habitacion.java
@@ -0,0 +1,14 @@
+package composicion;
+
+public class Habitacion {
+    private String nombre;
+
+    public Habitacion(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+    
+}
